b21839b52c528d905c71149b6494ddfafe56a79c
Mogbolahan Folarin <sahmiel@outlook.com>
2022-09-16 14:12:17 -0700

    iSupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,14 +11,13 @@
 
 int _isupper(int c)
 {
-		int i = isupper(c);
-
-			if (i > 0)
-					{
-								return (1);
-									}
-				else
-						{
-									return (0);
-										}
+	int i = isupper(c);
+		if (i > 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 }
