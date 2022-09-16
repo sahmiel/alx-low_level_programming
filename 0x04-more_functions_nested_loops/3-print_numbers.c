19f309038e8927a7e805cd6edd17d83a38fcd952
Mogbolahan Folarin <sahmiel@outlook.com>
2022-09-16 14:31:21 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * print_numbers - function that print all numbers from
+ * 0 to 9
  *
- * Return: Always 0.
+ * Return: returns nothing
  */
-int main(void)
-{
-		print_numbers();
 
-			return (0);
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
 }
