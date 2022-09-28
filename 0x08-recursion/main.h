de464c07ab36fa8ec844c93aae65c0c283542479
Mogbolahan Folarin <sahmiel@outlook.com>
2022-09-28 15:08:46 -0700

    Main Header

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -7,4 +13,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2);
+
+
+#endif /MAIN_H/
