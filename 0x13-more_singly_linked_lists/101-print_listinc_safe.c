3ad641bef175fd41a8acde4370e1c750cf535fbc
Mogbolahan Folarin <sahmiel@outlook.com>
2022-10-25 03:34:49 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listinc_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listinc_safe.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *  * print_listint_safe - prints a linked list, safely
- *   * @head: list of type listint_t to print
- *    *
- *     * Return: number of nodes in the list
- *      */
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ *
+ * Return: number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
 	    size_t num = 0;
--- 0x13-more_singly_linked_lists/102-free_listinc_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listinc_safe.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *  * free_listp2 - frees a linked list
- *   * @head: head of a list.
- *    *
- *     * Return: no return.
- *      */
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
 void free_listp2(listp_t **head)
 {
 		listp_t *temp;
@@ -24,11 +24,11 @@ void free_listp2(listp_t **head)
 }
 
 /**
- *  * free_listint_safe - frees a linked list.
- *   * @h: head of a list.
- *    *
- *     * Return: size of the list that was freed.
- *      */
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
 {
 		size_t nnodes = 0;
